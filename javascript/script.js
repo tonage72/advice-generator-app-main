25a2e80de67433baa1f8d9340d892e2bf9dda435
David Fleming <tonage72@gmail.com>
2024-03-02 23:30:34 -0500

    Just need to do advice #

---
+++ javascript/script.js
@@ -0,0 +1,16 @@
+const adviceNumber = document.querySelector(".advice-number");
+const adviceText = document.querySelector(".advice-text");
+const diceButton = document.querySelector(".dice-button");
+
+diceButton.addEventListener("click", getAdvice);
+
+function getAdvice() {
+	fetch("https://api.adviceslip.com/advice")
+	  .then(res => res.json())
+	  .then(res => {
+		adviceText.innerHTML = "\"" + res.slip.advice + "\"";
+	  })
+	  .catch(err => console.error(err));
+}
+
+getAdvice();
